5f6cf65305b1dd861cc73ee6a94d481b59c13494
sql3 <kim.va@edu.spbstu.ru>
2023-02-07 00:39:14 +0300

    Реализовал методы save, get, delete, clear, getAll, size в классе ArrayStorage. Проверил их реализацию c помощью классов MainArray.main() и MainTestArrayStorage.main()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,81 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        boolean nullInStorage = false;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                nullInStorage = true;
+                break;
+            }
+        }
+        if (!nullInStorage) {
+            System.err.println("The storage is full");
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume i : storage) {
+            if (i == null) {
+                break;
+            }
+            if (i.uuid.equals(uuid)) {
+                return i;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int box = -1;
+        int firstNullIndex = storage.length;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                firstNullIndex = i;
+                break;
+            } else if (storage[i].uuid.equals(uuid)) {
+                box = i;
+                break;
+            }
+        }
+        if (box == -1) {
+            System.err.println("No resume was found");
+        }
+        Resume[] copy = Arrays.copyOfRange(storage, box + 1, firstNullIndex);
+        for (int i = box, j = 0; i < copy.length; i++, j++) {
+            if (storage[i] == null) {
+                break;
+            }
+            storage[i] = copy[j];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int box = storage.length;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                box = i;
+                break;
+            }
+        }
+        return Arrays.copyOfRange(storage, 0, box);
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume i : storage) {
+            if (i == null) {
+                return size;
+            }
+            size++;
+        }
+        return size;
     }
 }
