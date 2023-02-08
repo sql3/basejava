fb1c61a590133f0e245281ac7dcf5355a0df3195
sql3 <kim.va@edu.spbstu.ru>
2023-02-08 21:55:50 +0300

    Добавил поле size; Изменил метод size(); В методе clear изменил метод fill(добавил начальный и конечный индексы).

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,11 +6,15 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    int size = 0;
+
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        size++;
         boolean nullInStorage = false;
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
@@ -37,6 +41,7 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
+        size--;
         int box = -1;
         int firstNullIndex = storage.length;
         for (int i = 0; i < storage.length; i++) {
@@ -50,6 +55,7 @@ public class ArrayStorage {
         }
         if (box == -1) {
             System.err.println("No resume was found");
+            size++;
         }
         Resume[] copy = Arrays.copyOfRange(storage, box + 1, firstNullIndex);
         for (int i = box, j = 0; i < copy.length; i++, j++) {
@@ -75,13 +81,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int size = 0;
-        for (Resume i : storage) {
-            if (i == null) {
-                return size;
-            }
-            size++;
-        }
         return size;
     }
 }
